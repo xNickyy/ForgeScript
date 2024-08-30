d8383316231cdbddc8c7d7e12af449cf313ad542
Nicky <129659754+xNickyy@users.noreply.github.com>
2024-08-30 18:05:58 +0000

    Added $isSlashSubCommand

---
+++ dist/native/interaction/isSlashSubCommand.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const structures_1 = require("../../structures");
+exports.default = new structures_1.NativeFunction({
+    name: "$isSlashSubCommand",
+    version: "1.5.0",
+    description: "Whether the interaction is a slash sub command",
+    unwrap: false,
+    output: structures_1.ArgType.Boolean,
+    execute(ctx) {
+        return this.success(ctx.interaction?.isChatInputCommand() && ctx.interaction.commandType === 1 || 2);
+    },
+});
+//# sourceMappingURL=isSlashSubCommand.js.map
---
+++ src/native/interaction/isSlashSubCommand.ts
@@ -0,0 +1,12 @@
+import { ArgType, NativeFunction, Return } from "../../structures"
+
+export default new NativeFunction({
+    name: "$isSlashSubCommand",
+    version: "1.5.0",
+    description: "Whether the interaction is a slash sub command",
+    unwrap: false,
+    output: ArgType.Boolean,
+    execute(ctx) {
+        return this.success(ctx.interaction?.isChatInputCommand() && ctx.interaction.commandType === 1 || 2)
+    },
+})
